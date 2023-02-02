6c2dc15634814d0f7472a3362f39d7fc329d7d6f
J.C Garcia <77996735+juancarlos3017@users.noreply.github.com>
2023-02-02 10:53:10 -0500

    Create languaje.js
    Spanish language was added to existing file

---
+++ languaje.js
@@ -0,0 +1,173 @@
+{
+  "he-IL": {
+    "btn_open": "תפריט נגישות",
+    "btn_close": "סגור",
+    "keyboard_root": "ניווט מקלדת",
+    "disable_animattions": "חסימת אנימציות",
+    "access_declaration": "הצהרת נגישות",
+    "debug_contacts": "דווח על בעיית נגישות",
+    "reset_all_settings": "בטל נגישות",
+    "image_without_alt": "תמונה ללא תיאור",
+    "contrast_block": {
+      "header": "ניגודיות צבעים",
+      "btn_monochrome": "תצוגת<br>מונוכרום",
+      "btn_bright": "ניגודיות<br>בהירה",
+      "btn_invert": "ניגודיות<br>הפוכה"
+    },
+    "text_block": {
+      "header": "גודל טקסט",
+      "btn_font_up": "הגדלת<br>גופון",
+      "btn_font_down": "הקטנת<br>גופון",
+      "btn_font_readable": "גופון<br>קריא"
+    },
+    "content_block": {
+      "header": "הדגשת תוכן",
+      "btn_underline_links": "הדגשת<br>קישורים",
+      "btn_underline_headers": "הדגשת<br>כותרות",
+      "btn_images_titles": "תיאור<br>לתמונות"
+    },
+    "zoom_block": {
+      "header": "הגדלת תצוגה",
+      "btn_cursor_white": "סמן לבן<br>גדול",
+      "btn_cursor_black": "סמן שחור<br>גדול",
+      "btn_zoom_in": "הגדלת<br>תצוגה"
+    }
+  },
+  "ru-RU": {
+    "btn_open": "Меню доступности",
+    "btn_close": "Закрыть",
+    "keyboard_root": "Навигация с клавиатуры",
+    "disable_animattions": "Блокировать анимацию",
+    "access_declaration": "Декларация доступности",
+    "debug_contacts": "Сообщить о проблеме",
+    "reset_all_settings": "Сбросить настройки",
+    "image_without_alt": "изображение без текста",
+    "contrast_block": {
+      "header": "Цветовой контраст",
+      "btn_monochrome": "монохр. дисплей",
+      "btn_bright": "светлый контраст",
+      "btn_invert": "инверт. контраст"
+    },
+    "text_block": {
+      "header": "Размер текста",
+      "btn_font_up": "увелич. шрифта",
+      "btn_font_down": "уменьш. шрифта",
+      "btn_font_readable": "читаемый шрифт"
+    },
+    "content_block": {
+      "header": "Выделение содержимого",
+      "btn_underline_links": "выдел. ссылки",
+      "btn_underline_headers": "выдел. заголовки",
+      "btn_images_titles": "названия картинок"
+    },
+    "zoom_block": {
+      "header": "Увеличение контента",
+      "btn_cursor_white": "белый курсор",
+      "btn_cursor_black": "черный курсор",
+      "btn_zoom_in": "увелич. дисплея"
+    }
+  },
+  "en": {
+    "btn_open": "accessibility menu",
+    "btn_close": "close",
+    "keyboard_root": "keyboard navigation",
+    "disable_animattions": "block animations",
+    "access_declaration": "accessibility statement",
+    "debug_contacts": "report an accessibility problem",
+    "reset_all_settings": "reset settings",
+    "image_without_alt": "image without text",
+    "contrast_block": {
+      "header": "color contrast",
+      "btn_monochrome": "uncolored<br>display",
+      "btn_bright": "bright<br>contrast",
+      "btn_invert": "invert<br>colors"
+    },
+    "text_block": {
+      "header": "text size",
+      "btn_font_up": "increase<br>text",
+      "btn_font_down": "decrease<br>text",
+      "btn_font_readable": "readable<br>text"
+    },
+    "content_block": {
+      "header": "highlighting content",
+      "btn_underline_links": "underline<br>links",
+      "btn_underline_headers": "underline<br>headers",
+      "btn_images_titles": "images<br>titles"
+    },
+    "zoom_block": {
+      "header": "zoom in",
+      "btn_cursor_white": "big white<br>cursor",
+      "btn_cursor_black": "big black<br>cursor",
+      "btn_zoom_in": "zoom<br>screen"
+    }
+  },
+  "fr_FR": {
+    "btn_open": "menu d'accessibilité",
+    "btn_close": "fermer",
+    "keyboard_root": "navigation au clavier",
+    "disable_animattions": "désactiver les animations",
+    "access_declaration": "déclaration d'accessibilité",
+    "debug_contacts": "signaler un problème d'accessibilité",
+    "reset_all_settings": "réinitialiser les paramètres",
+    "image_without_alt": "image sans texte",
+    "contrast_block": {
+      "header": "contraste des couleurs",
+      "btn_monochrome": "affichage<br>sans couleur",
+      "btn_bright": "contraste<br>lumineux",
+      "btn_invert": "couleurs<br>inversées"
+    },
+    "text_block": {
+      "header": "taille de police",
+      "btn_font_up": "augmenter<br>la taille",
+      "btn_font_down": "diminuer<br>la taille",
+      "btn_font_readable": "texte<br>lisible"
+    },
+    "content_block": {
+      "header": "mettre en évidence le contenu",
+      "btn_underline_links": "souligner<br>les liens",
+      "btn_underline_headers": "souligner<br>les en-têtes",
+      "btn_images_titles": "titres<br>d'images"
+    },
+    "zoom_block": {
+      "header": "zoomer",
+      "btn_cursor_white": "gros curseur<br>blanc",
+      "btn_cursor_black": "gros curseur<br>noir",
+      "btn_zoom_in": "élargir<br>l'écran"
+    }
+  },
+  "es": {
+    "btn_open": "menu de accesibilidad",
+    "btn_close": "cerrar",
+    "keyboard_root": "tecaldo de navigacion",
+    "disable_animattions": "quitar animacion",
+    "access_declaration": "accesibilidad statement",
+    "debug_contacts": "reportar problema de accesibilidad",
+    "reset_all_settings": "reiniciar ajustes",
+    "image_without_alt": "imagenes sin texto",
+    "contrast_block": {
+      "header": "color contraste",
+      "btn_monochrome": "sin color<br>mostrar",
+      "btn_bright": "brillo<br>contraste",
+      "btn_invert": "invertir<br>colores"
+    },
+    "text_block": {
+      "header": "tamano del texto",
+      "btn_font_up": "aumentar<br>texto",
+      "btn_font_down": "disminuir<br>texto",
+      "btn_font_readable": "legible<br>texto"
+    },
+    "content_block": {
+      "header": "destacar contenido",
+      "btn_underline_links": "subrayar<br>links",
+      "btn_underline_headers": "subrayar<br>encabezados",
+      "btn_images_titles": "imageness<br>titulos"
+    },
+    "zoom_block": {
+      "header": "aumentar",
+      "btn_cursor_white": "grande blanco<br>cursor",
+      "btn_cursor_black": "grande negro<br>cursor",
+      "btn_zoom_in": "aumentar<br>pantalla"
+    }
+
+  }
+}
